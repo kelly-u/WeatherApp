af2da4790adade13035194ef37f05e390156fcf9
kelly-u <gleicekellymacedodd@gmail.com>
2024-12-10 22:14:11 -0300

    Parte 1 da prática 3. Estão aparecendo a lista de cidades em favoritos.

--- app/src/main/java/com/weatherapp/ui/ListPage.kt
+++ app/src/main/java/com/weatherapp/ui/ListPage.kt
@@ -1,34 +1,101 @@
 package com.weatherapp.ui
 
-import androidx.compose.foundation.background
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.rounded.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.weatherapp.R
+import com.weatherapp.MainActivity
+
+data class City (
+    val name : String,
+    val weather: String? = null,
+    val location: String? = null
+)
+
+private fun getCities() = List(20) { i ->
+    City(name = "Cidade $i", weather = "Carregando clima...")
+}
 
 @Composable
-fun ListPage() {
-    Column(
-        modifier = Modifier.fillMaxSize()
-            .background(colorResource(id = R.color.purple_200))
-            .wrapContentSize(Alignment.Center)
+fun CityItem(
+    city: City,
+    onClick: () -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth().padding(8.dp).clickable { onClick() },
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(
-            text = "Favoritas",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+        Icon(
+            Icons.Rounded.FavoriteBorder,
+            contentDescription = ""
         )
+        Spacer(modifier = Modifier.size(12.dp))
+        Column(modifier = modifier.weight(1f)) {
+            Text(modifier = Modifier,
+                text = city.name,
+                fontSize = 24.sp)
+            Text(modifier = Modifier,
+                text = city.weather?:"Carregando clima...",
+                fontSize = 16.sp)
+        }
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+}
+
+@SuppressLint("RememberReturnType")
+@Composable
+fun ListPage(modifier: Modifier = Modifier) {
+    val cityList = remember { getCities().toMutableStateList() }
+    val activity = LocalContext.current as? Activity
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        items(cityList) { city ->
+            CityItem(city = city, onClose = {
+                Toast.makeText(activity, "Removendo cidade", Toast.LENGTH_LONG).show()
+                activity?.startActivity(
+                    Intent(activity, MainActivity::class.java).setFlags(
+                        Intent.FLAG_ACTIVITY_SINGLE_TOP
+                    )
+                )
+            }, onClick = {
+                Toast.makeText(activity, "Visualizar cidade", Toast.LENGTH_LONG).show()
+                activity?.startActivity(
+                    Intent(activity, MainActivity::class.java).setFlags(
+                        Intent.FLAG_ACTIVITY_SINGLE_TOP
+                    )
+                )
+            })
+        }
     }
 }
